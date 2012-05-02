62ae54de981be36d89dd8b2b46eee08ff0e94f7b
Eric Keary <eric.keary@gmail.com>
2012-05-02 13:45:49 -0600

    Fixed bugs with top clipping and added click handlers to text inside symbol

--- src/cay.js
+++ src/cay.js
@@ -26,77 +26,102 @@ function drawUniqueSubPoints(r, uniqueSubPoints) {
      *
      */
 
-    xOrigin = 310;
-    yOrigin = 212;
-    radius = 200;
-    theta = 360 / uniqueSubPoints.length;
-    angle = 0;
+    var xOrigin = 310;
+    var yOrigin = 250;
+    var radius = 200;
+    var theta = 360 / uniqueSubPoints.length;
+    var angle = 0;
 
-    for (i = 0; i < uniqueSubPoints.length; i++) {
+    for (var i = 0; i < uniqueSubPoints.length; i++) {
 
-        xOffset = radius * Math.cos(angle) + xOrigin;
-        yOffset = radius * Math.sin(angle) + yOrigin;
+        var xOffset = radius * Math.cos(angle) + xOrigin;
+        var yOffset = radius * Math.sin(angle) + yOrigin;
 
-        r.circle(xOffset, yOffset, 50).attr({fill: '#7a3e48'}).node.onclick = function () {
-            r.clear();
-        };
+        r.circle(xOffset, yOffset, 50)
+            .attr({fill: '#7a3e48'})
+            .node.onclick = function () {
+                r.clear();
+            };
+
         r.text(xOffset, yOffset, uniqueSubPoints[i])
-            .attr({font:'12px  "Lucida Sans Unicode", "Lucida Grande", sans-serif', fill:"#eecd86"});
+            .attr({
+                font:'12px  "Lucida Sans Unicode", "Lucida Grande", sans-serif',
+                fill:"#eecd86"
+            });
 
         angle += (theta * 4);
     }
 }
 
+function showSubPoints(r, points) {
+    var subPoints = $.map(points, function (point) {
+        return point.name
+            .slice(0, point.name
+                .lastIndexOf('.'));
+    });
+
+    drawUniqueSubPoints(r, unique(subPoints));
+}
+
 function drawUniquePoints(r, uniquePoints, points) {
-    r.circle(310, 212, 100).attr({fill: '#7a3e48'}).node.onclick = function () {
+    r.circle(310, 250, 100)
+        .attr({fill: '#7a3e48'})
+        .node.onclick = function () {
+            showSubPoints(r, points);
+        };
 
-        var subPoints = $.map(points, function (point) {
-            return point.name.slice(0, point.name.lastIndexOf('.'));
+    r.text(310, 250, uniquePoints[0])
+        .attr({
+            font:'24px  "Lucida Sans Unicode", "Lucida Grande", sans-serif',
+            fill:"#eecd86"
+        })
+        .node.onclick = function () {
+            showSubPoints(r, points);
+        };
+}
+
+function launchCay(r) {
+    r.clear();
+
+    var client = $.reefClient({
+        'server':'http://69.164.158.23:7889',
+        'error_div':$('#error_div'),
+        'auto_login':{
+            'name':'guest', 'password':'guest'
+        }
+    });
+
+    client.getPoints()
+        .done(function (points) {
+            var pointNames = $.map(points, function (point) {
+                return point.name
+                    .slice(0, point.name
+                        .indexOf('.'));
+            });
+
+            drawUniquePoints(r, unique(pointNames), points);
         });
-
-        drawUniqueSubPoints(r, unique(subPoints));
-    };
-    r.text(310, 212, uniquePoints[0])
-        .attr({font:'24px  "Lucida Sans Unicode", "Lucida Grande", sans-serif', fill:"#eecd86"});
 }
 
 $(document).ready(function() {
-    var r = new Raphael("container", 619, 719),
-        dashed = {fill: "none", stroke: "#666", "stroke-dasharray": "- "};
+    var r = new Raphael("container", 620, 700);
 
-    function launchButton() {
-        r.rect(210, 185, 200, 50, 5).attr({fill: '#7a3e48'}).node.onclick = function () {
-            r.clear();
+    function launchButton(r) {
+        r.rect(210, 185, 200, 50, 5)
+            .attr({fill: '#7a3e48'})
+            .node.onclick = function() {
+                launchCay(r);
+            };
 
-            var client = $.reefClient({
-                'server':'http://69.164.158.23:7889',
-                'error_div':$('#error_div'),
-                'auto_login':{
-                    'name':'guest', 'password':'guest'
-                }
-            });
-
-            client.getPoints().done(function (points) {
-                var pointNames = $.map(points, function (point) {
-                    return point.name.slice(0, point.name.indexOf('.'));
-                    //return point.name
-                });
-
-            drawUniquePoints(r, unique(pointNames), points);
-
-                /*
-                 if (pointNames.length > 0) {
-                 $('#data_div').displayMeasurements({
-                 'client':client,
-                 'point_names':pointNames,
-                 'polling':{'enabled':false, 'period':1000}
-                 });
-                 } */
-            });
-        };
         r.text(310, 212, "Launch Cay")
-            .attr({font:'24px  "Lucida Sans Unicode", "Lucida Grande", sans-serif', fill:"#eecd86"});
+            .attr({
+                font:'24px  "Lucida Sans Unicode", "Lucida Grande", sans-serif',
+                fill:"#eecd86"
+            })
+            .node.onclick = function() {
+                launchCay(r);
+            };
     }
 
-    launchButton();
+    launchButton(r);
 });
